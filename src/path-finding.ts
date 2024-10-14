d9ae228cd25f9231db026a294c847f6f69890de9
Karyl Chesman <karylbps@gmail.com>
2024-10-14 19:02:33 -0300

    fix: the cost of each neighbor wasn't being correctly calculated

--- src/path-finding.ts
+++ src/path-finding.ts
@@ -1,6 +1,8 @@
 import { Vector2 } from "three";
 import { World } from "./world";
 
+type CostMap = Map<string, number>;
+
 function getKey(coords: Vector2) {
   return `${coords.x}-${coords.y}`;
 }
@@ -19,9 +21,11 @@ export function search(
 
   let path_found = false;
   const max_search_distance = 20;
+
   const came_from = new Map();
-  const visited = new Set<string>();
+  const cost: CostMap = new Map();
   const frontier = [start];
+  cost.set(getKey(start), 0);
 
   while (frontier.length > 0) {
     // Get the square with the shortest distance metric
@@ -42,15 +46,12 @@ export function search(
       break;
     }
 
-    // Mark this square as visited
-    visited.add(getKey(candidate));
-
     if (candidate.manhattanDistanceTo(start) > max_search_distance) {
       continue;
     }
 
     // Search the neighbors of the square
-    const neighbors = getNeighbors(candidate, world, visited);
+    const neighbors = getNeighbors(candidate, world, cost);
     frontier.push(...neighbors);
 
     // Mark which square each neighbor came from
@@ -77,7 +78,7 @@ export function search(
   return path;
 }
 
-function getNeighbors(coords: Vector2, world: World, visited: Set<string>) {
+function getNeighbors(coords: Vector2, world: World, cost: CostMap) {
   const neighbors: Vector2[] = [];
 
   // Left
@@ -97,9 +98,22 @@ function getNeighbors(coords: Vector2, world: World, visited: Set<string>) {
     neighbors.push(new Vector2(coords.x, coords.y + 1));
   }
 
+  // Cost to get to neighbor square is the current square cost + 1
+  const new_cost = (cost.get(getKey(coords)) || 0) + 1;
+
   // Exclude any squares that are already visited, as well
   // as any squares that are occupied
   return neighbors
-    .filter((coords) => !visited.has(getKey(coords)))
-    .filter((coords) => !world.getObject(coords));
+    .filter((coords_item) => {
+      // If neighboring square has not yet been visited, or this
+      // is a cheaper path cost, then include it in the search
+      const coords_cost = cost.get(getKey(coords_item));
+      if (!coords_cost || new_cost < coords_cost) {
+        cost.set(getKey(coords_item), new_cost);
+        return true;
+      }
+
+      return false;
+    })
+    .filter((coords_item) => !world.getObject(coords_item));
 }
