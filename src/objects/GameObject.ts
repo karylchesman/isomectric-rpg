9e20051937f41338f8045833ce15f513e50b3e95
Karyl Chesman <karylbps@gmail.com>
2024-12-24 16:50:22 -0300

    implemented the hit points overlay

--- src/objects/GameObject.ts
+++ src/objects/GameObject.ts
@@ -1,11 +1,39 @@
-import { BufferGeometry, Material, Mesh, Vector3 } from "three";
+import { BufferGeometry, Material, Mesh, Sprite, Vector3 } from "three";
+import { createTextMaterial } from "../utils";
 
 export class GameObject extends Mesh {
   coords: Vector3;
+  private _hit_points: number = 10;
+  private _max_hit_points: number = 10;
+  private _health_overlay: Sprite;
 
   constructor(coords: Vector3, geometry: BufferGeometry, material: Material) {
     super(geometry, material);
     this.coords = coords;
+
+    this._health_overlay = new Sprite();
+    this._health_overlay.position.y = 0.75;
+    this.add(this._health_overlay);
+
+    this.updateHitPointOverlay();
+  }
+
+  get hit_points() {
+    return this._hit_points;
+  }
+  get max_hit_points() {
+    return this._max_hit_points;
+  }
+  get is_dead() {
+    return this._hit_points === 0;
+  }
+
+  hit(damage: number) {
+    this._hit_points -= damage;
+    if (this._hit_points < 0) {
+      this._hit_points = 0;
+    }
+    this.updateHitPointOverlay();
   }
 
   moveTo(coords: Vector3) {
@@ -14,6 +42,15 @@ export class GameObject extends Mesh {
       this.coords.x + 0.5,
       this.coords.y + 0.5,
       this.coords.z + 0.5
+    );
+  }
+
+  updateHitPointOverlay() {
+    if (this._health_overlay.material) {
+      this._health_overlay.material.dispose();
+    }
+    this._health_overlay.material = createTextMaterial(
+      `${this._hit_points}/${this.max_hit_points}`
     );
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,35 @@
-import { Vector3 } from "three";
+import { CanvasTexture, SpriteMaterial, Vector3 } from "three";
 
 /**
  * Returns the key for the object map given a set of coordinates
  */
 export function getObjectMapKey(coords: Vector3) {
   return `${coords.x}-${coords.y}-${coords.z}`;
+}
+
+/**
+ * @param text the text that we want to render
+ */
+export function createTextMaterial(text: string): SpriteMaterial {
+  const size = 512;
+  const canvas = document.createElement("canvas");
+  canvas.width = size;
+  canvas.height = size;
+
+  const context = canvas.getContext("2d");
+  if (!context) throw new Error("Unable to access canvas context");
+  context.font = "100px Arial";
+  context.textBaseline = "middle";
+  context.textAlign = "center";
+
+  context.strokeStyle = "black";
+  context.lineWidth = 4; // 4 pixels;
+  context.fillStyle = "white";
+
+  context.strokeText(text, size / 2, size / 2);
+  context.fillText(text, size / 2, size / 2);
+
+  return new SpriteMaterial({
+    map: new CanvasTexture(canvas),
+  });
 }
