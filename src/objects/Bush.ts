c29d53e293d9e2ff98d427c78b71288af53584f0
Karyl Chesman <karylbps@gmail.com>
2025-02-22 17:21:37 -0300

    added bush model to the Bush object class and replaced grid texture by one darker

--- src/objects/Bush.ts
+++ src/objects/Bush.ts
@@ -1,25 +1,33 @@
-import { Mesh, MeshStandardMaterial, SphereGeometry, Vector3 } from "three";
+import { Mesh, MeshStandardMaterial, Vector3 } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { getModelPath } from "../utils";
 import { GameObject } from "./GameObject";
+
+const loader = new GLTFLoader();
 
 const BUSH_MATERIAL = new MeshStandardMaterial({
   color: 0x80a040,
   flatShading: true,
 });
-const BUSH_GEOMETRY = new SphereGeometry(1, 8, 8);
 
 export class Bush extends GameObject {
   constructor(coords: Vector3) {
-    const min_bush_radius = 0.1;
-    const max_bush_radius = 0.3;
-    const radius =
-      min_bush_radius + Math.random() * (max_bush_radius - min_bush_radius);
-
-    const bush_mesh = new Mesh(BUSH_GEOMETRY, BUSH_MATERIAL);
-    bush_mesh.scale.set(radius, radius, radius);
-    bush_mesh.position.set(0.5, radius, 0.5);
-
-    super(coords, bush_mesh);
+    super(coords, new Mesh());
 
     this.name = `Bush-(${coords.x},${coords.z})`;
+
+    loader.load(getModelPath(`bush1`), (bush_model) => {
+      const variation = new Vector3(0, 0.1, 0);
+      // @ts-ignore
+      this.mesh.add(bush_model.scene.children[0]);
+      this.mesh.scale.set(
+        1.0 + 2.0 * variation.x * (Math.random() - 0.5),
+        1.0 + 2.0 * variation.y * (Math.random() - 0.5),
+        1.0 + 2.0 * variation.z * (Math.random() - 0.5)
+      );
+      this.mesh.rotation.set(0, Math.random() * 2 * Math.PI, 0);
+      this.mesh.position.set(0.5, 0, 0.5);
+      this.mesh.material = BUSH_MATERIAL;
+    });
   }
 }
